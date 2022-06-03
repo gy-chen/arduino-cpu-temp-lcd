c6554fd48aad0a78e35adc5dec9b5b35ee4a3e6d
gy-chen <gychen@gyhost.icu>
2022-06-03 15:37:57 +0800

    Update sketch_lcd_cpu_temp.ino

--- sketches/sketch_lcd_cpu_temp.ino
+++ sketches/sketch_lcd_cpu_temp.ino
@@ -1,11 +1,15 @@
-const int PWM_INPUT_PIN = A4;
+#include <LiquidCrystal_PCF8574.h>
+
+const int PWM_INPUT_PIN = A2;
 
 const float MIN_DUTY_CYCLE = 0.2;
 const float MAX_DUTY_CYCLE = 1;
 
 const int MOVING_AVG_NUM = 100;
 
+LiquidCrystal_PCF8574 lcd(0x27);
 double cpu_temp = -1;
+double last_lcd_update = millis();
 
 double to_duty_cycle(long high_duration_microsecond) {
   // NOTE: fine tune this value for your system
@@ -33,14 +37,38 @@ double update_cpu_temp(double cpu_temp, double new_cpu_temp) {
   }
 }
 
+void setup_lcd() {
+   lcd.begin(16, 2);
+   lcd.setBacklight(0);
+}
+
+void update_lcd(String line1, String line2) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(line1);
+  lcd.setCursor(0, 1);
+  lcd.print(line2);
+}
+
+String format_cpu_temp(double cpu_temp) {
+   char buffer[9];
+   sprintf(buffer, "  %s C", dtostrf(cpu_temp, 2, 0, "%f"));
+   return String(buffer);
+}
+
+void display_cpu_temp(double cpu_temp) {
+  if (abs(millis() - last_lcd_update) > 2000) {
+    update_lcd("CPU Temperature:", format_cpu_temp(cpu_temp));
+    last_lcd_update = millis();
+  }
+}
+
 void setup() {
-  Serial.begin(9600);
+  setup_lcd();
 }
 
 void loop() {
   double new_cpu_temp = get_cpu_temp(PWM_INPUT_PIN);
   cpu_temp = update_cpu_temp(cpu_temp, new_cpu_temp);
-  
-  Serial.println(cpu_temp);
-  delay(25);
+  display_cpu_temp(cpu_temp);
 }
